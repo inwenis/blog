167b129d3093fd3fc8a7b5fa787b4217d731680d
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2024-05-18 10:20:44 +0200

    improve example

--- post7/server.mjs
+++ post7/server.mjs
@@ -5,16 +5,23 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-      "Set-Cookie": `my_cookie=oreo`,
-      "Content-Type": `text/html`,
-      'Access-Control-Allow-Origin': 'null'
-  });
-  const x = fs.readFileSync('test.html');
-  res.write(x);
-  res.end();
+    if (req.url === '/') {
+        res.writeHead(200, {
+            "Content-Type": `text/html`,
+        });
+        const x = fs.readFileSync('test.html');
+        res.write(x);
+        res.end();
+    }
+    if (req.url === '/with-cookie') {
+        res.writeHead(200, {
+            "Set-Cookie": `my_cookie=oreo`,
+            "Content-Type": `text/html`,
+        });
+        res.end();
+    }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
