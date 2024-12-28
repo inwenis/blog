34e34a752b82220e6fa4a70fd845c4a3824cbab4
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2024-12-28 13:32:36 +0100

    wip

--- wip/post19/makeLogs.fsx
+++ wip/post19/makeLogs.fsx
@@ -5,9 +5,12 @@ open System.Text.Json
 #r "nuget: FSharp.Data"
 open FSharp.Data
 
+fsi.AddPrinter<DateTimeOffset>(fun dt -> dt.ToString("O"))
+
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__ // ensures the script runs from the directory it's located in
 
 // -------------------------------------------------------------------------
+
 
 type LogEntry = {
     Timestamp: DateTime
@@ -189,3 +192,55 @@ JsonNode.DeepEquals(x, y) // comparing json
 // The System.Text.Json namespace contains all the entry points and the main types.
 // The System.Text.Json.Serialization namespace contains attributes and APIs for advanced scenarios and customization specific to serialization and deserialization.
 // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/use-dom#json-dom-choices
+
+
+type RecordTest = {
+    Timestamp: DateTimeOffset
+    Level: string
+    }
+
+let r = { Timestamp = DateTimeOffset.Now; Level = "ERROR" }
+
+JsonSerializer.Serialize(r) |> JsonSerializer.Deserialize<RecordTest>
+
+let tupleT = (DateTimeOffset.Now, "ERROR")
+type TupleAlias = string * float
+let tuple:TupleAlias = ("asdf", 1.0)
+JsonSerializer.Serialize(tuple) |> JsonSerializer.Deserialize<TupleAlias>
+JsonSerializer.Serialize((1,2)) |> JsonSerializer.Deserialize<int*int>
+
+type UnionTest =
+    | A of int
+    | B of string
+
+let u = A 1
+
+JsonSerializer.Serialize(u) // eeeeeeeeeeeeee !
+
+type RecordTest2 = {
+    Timestamp: DateTimeOffset
+    Level: string
+    TestOp: int option
+    }
+
+let r2 = { Timestamp = DateTimeOffset.Now; Level = "ERROR"; TestOp = Some 1 }
+JsonSerializer.Serialize(r2) |> JsonSerializer.Deserialize<RecordTest2>
+let r3 = { Timestamp = DateTimeOffset.Now; Level = "ERROR"; TestOp = None }
+JsonSerializer.Serialize(r3) |> JsonSerializer.Deserialize<RecordTest2>
+
+
+#r "nuget: FSharp.Json"
+
+open FSharp.Json
+
+type TheUnion =
+| OneFieldCase of string
+| ManyFieldsCase of string*int
+
+let data = OneFieldCase "The string"
+
+let json = Json.serialize data
+// json is """{"OneFieldCase":"The string"}"""
+
+let deserialized = Json.deserialize<TheUnion> json
+// deserialized is OneFieldCase("The string")
