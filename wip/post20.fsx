a40134efb6b3eff994e07f2d9e63988b248af57e
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2024-12-28 13:16:02 +0100

    add post 20 wip

---
+++ wip/post20.fsx
@@ -0,0 +1,10 @@
+open System
+open System.IO
+open System.Text.Json
+
+#r "nuget: FSharp.Data"
+open FSharp.Data
+
+fsi.AddPrinter<DateTimeOffset>(fun dt -> dt.ToString("O"))
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__ // ensures the script runs from the directory it's located in
