032bd3b1aa96fcae8c9dbda67d4e94f9f8665b60
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2024-12-29 11:52:12 +0100

    wip

--- wip/post19/makeLogs.fsx
+++ wip/post19/makeLogs.fsx
@@ -257,6 +257,30 @@ let json = Json.serialize data
 let deserialized = Json.deserialize<TheUnion> json
 // deserialized is OneFieldCase("The string")
 
-// I could also test this
+
+
+
 let j = FSharp.Data.JsonValue.Parse("{}")
 j.Properties()
+
+
+let d =
+    JsonValue.Record [|
+        "event",      JsonValue.String "asdf"
+        "properties", JsonValue.Record [|
+            "token",       JsonValue.String "tokenId"
+            "distinct_id", JsonValue.String "123123"
+        |]
+    |]
+
+d.ToString().Replace("\r\n", "").Replace(" ", "")
+
+
+// https://stackoverflow.com/questions/48113281/how-to-create-nested-json-objects-in-fsharp
+// maybe update answer here
+
+
+
+
+
+
